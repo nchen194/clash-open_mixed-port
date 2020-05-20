7a8e3043c4f550aae8e11f22a8a573e57f72eb92
nchen194 <47018658+nchen194@users.noreply.github.com>
2020-05-20 16:34:26 +0800

    Update open_mixed-port.sh

--- open_mixed-port.sh
+++ open_mixed-port.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+echo "输入你需要代理的端口号"
+read port
 sed -i 's/port: 7890/#port: 7890/g' /home/clash/.config/clash/config.yaml
 sed -i 's/socks-port: 7891/#socks-port: 7891/g' /home/clash/.config/clash/config.yaml
-echo -e "mixed-port: 7890" >> /home/clash/.config/clash/config.yaml
+echo -e "mixed-port: ${port}" >> /home/clash/.config/clash/config.yaml
 systemctl restart clash@clash.service
-
