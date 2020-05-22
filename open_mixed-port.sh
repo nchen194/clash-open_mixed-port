802edbe422cc08006a021538d8e754fdc5f5f3a4
nchen194 <47018658+nchen194@users.noreply.github.com>
2020-05-22 15:59:06 +0800

    Update open_mixed-port.sh

--- open_mixed-port.sh
+++ open_mixed-port.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
-echo "输入你需要代理的端口号"
-read port
-echo "输入需要验证用户名"
-read user
-echo "输入需要验证密码"
-read password
-sed -i 's/port: 7890/#port: 7890/g' /home/clash/.config/clash/config.yaml
-sed -i 's/socks-port: 7891/#socks-port: 7891/g' /home/clash/.config/clash/config.yaml
-sed -i 's/mixed-port/#mixed-port/g' /home/clash/.config/clash/config.yaml
-echo -e "mixed-port: ${port}" >> /home/clash/.config/clash/config.yaml
-echo -e "authentication:\n  - ${user}:${password}" >> /home/clash/.config/clash/config.yaml
-systemctl restart clash@clash.service
+clear
+echo && echo -e "
+ ${Green_font_prefix}1.${Font_color_suffix} 开启mixed-port
+ ${Green_font_prefix}2.${Font_color_suffix} 设置用户名密码"
+read -p " 请输入数字 [0-11]:" num
+ case "$num" in
+	1)
+		echo "输入你需要代理的端口号"
+		read port
+		sed -i 's/port: 7890/#port: 7890/g' /home/clash/.config/clash/config.yaml
+		sed -i 's/socks-port: 7891/#socks-port: 7891/g' /home/clash/.config/clash/config.yaml
+		sed -i 's/mixed-port/#mixed-port/g' /home/clash/.config/clash/config.yaml
+		echo -e "mixed-port: ${port}" >> /home/clash/.config/clash/config.yaml
+	;;
+	2)
+		echo "输入需要验证用户名"
+		read user
+		echo "输入需要验证密码"
+		read password
+		echo -e "authentication:\n  - ${user}:${password}" >> /home/clash/.config/clash/config.yaml
+		systemctl restart clash@clash.service
+	;;
+	*)
+	clear
+	echo -e "${Error}:请输入正确数字 [1-2]"
+esac：
