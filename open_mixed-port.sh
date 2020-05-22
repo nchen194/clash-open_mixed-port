6d7b86bae7a43fac0fa1ca36dce9b647e32b12f5
nchen194 <47018658+nchen194@users.noreply.github.com>
2020-05-22 14:57:37 +0800

    Update open_mixed-port.sh

--- open_mixed-port.sh
+++ open_mixed-port.sh
@@ -7,6 +7,7 @@ echo "输入需要验证密码"
 read password
 sed -i 's/port: 7890/#port: 7890/g' /home/clash/.config/clash/config.yaml
 sed -i 's/socks-port: 7891/#socks-port: 7891/g' /home/clash/.config/clash/config.yaml
+sed -i 's/mixed-port/#mixed-port/g' /home/clash/.config/clash/config.yaml
 echo -e "mixed-port: ${port}" >> /home/clash/.config/clash/config.yaml
 echo -e "authentication:\n  - ${user}:${password}" >> /home/clash/.config/clash/config.yaml
 systemctl restart clash@clash.service
